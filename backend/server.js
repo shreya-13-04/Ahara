1f3263bc7e82f9374c7aaa7d78eaf6ea8871b538
shreya-13-04 <shreyabaala2004@gmail.com>
2026-02-14 13:31:05 +0530

    Configured backend API integration with ngrok endpoint

--- backend/server.js
+++ backend/server.js
@@ -26,9 +26,10 @@ app.get("/", (req, res) => {
 const PORT = process.env.PORT || 5000;
 
 if (process.env.NODE_ENV !== 'test') {
-  app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-  });
+  app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
 }
 
 module.exports = app;
